dbaac88c1ecf7aef0fc234ecf1c233269093d47b
zemimoh <zemimoh@users.noreply.github.com>
2016-11-30 18:47:06 +0100

    Add files via upload

---
+++ DashBoardNORTHWIND2/assets/js/locale/Northwind-ar.js
@@ -0,0 +1,100 @@
+(function ($) {
+    'use strict'; locales['ar'] = {
+        'language': 'اللغة',
+        'myLanguage': 'عربية',
+        'languageClass': 'fa fa-flag-o',
+        'dir': 'rtl',
+        'PowredBy': ' تصميم زموي محمد .&copy; 2016',
+        'Loading': 'تحميل البيانات',
+
+        'Stock': 'المخزون',
+        'Category': 'الصنف',
+        'Product': 'المنتج',
+        'Supplier': 'المورد',
+        'Supplier Country': 'بلد المورد',
+        'Supplier City': 'مدينة المورد',
+        'Units On Order': 'الكمية المطلوبة',
+        'Reorder Level': 'عتبة التزود',
+        'Unit Price': 'سعر الوحدة',
+        'Stock Sum': 'قيمة المخزون',
+
+        'Order': 'المبيعات',
+        'Order ID': 'الرقم',
+        'Employe': 'العامل',
+        'Employe Country': 'بلد العامل',
+        'Employe City': 'مدينة العامل',
+        'Customer ID': 'رقم الزبون',
+        'Customer': 'الزبون',
+        'Customer Country': 'بلد الزبون',
+        'Customer City': 'مدينة الزبون',
+        'Shipper': 'الناقل',
+        'Order Date': 'التاريخ',
+        'Year': 'السنة',
+        'Month': 'الشهر',
+
+
+        'Order Detail': 'تفاصيل المبيعات',
+        'Product ID': 'رقم السلعة',
+        'Quantity': 'الكمية',
+        'Discount': 'التخفيض',
+        'Extended Price': 'المبلغ الصافي',
+        'Profit': 'الربح',
+        'Debt': 'المستحقات',
+
+
+
+        'Data': 'البيانات',
+        'Price': 'المبلغ',
+        'Discount Price': 'قيمة التخفيض',
+        'Freight': 'مصاريف الشحن',
+
+
+        'About': 'التعريف بالبرنامج',
+        'Amount': 'المبلغ',
+
+        'Turnover': 'رقم الأعمال',
+        'CA Annuel': 'رقم الأعمال السنوي',
+        'Monthly Turnover': 'رقم الأعمال الشهري',
+        'CA Cumule': 'رقم الأعمال المتراكم',
+        'CA Commercial': 'رقم الأعمال حسب العامل',
+        'CA Client': 'رقم الأعمال حسب الزبون',
+        'CA Article': 'رقم الأعمال حسب السلع',
+        'CA Category': 'رقم الأعمال حسب الأصناف',
+        'Commercial': 'الممثل التجاري',
+
+        'Annuel Profit': 'الربح السنوي',
+        'Monthly Profit': 'الربح الشهري',
+        'Benefice Annuel': 'الربح السنوي',
+        'Benefice Cumule': 'الربح المتراكم',
+        'Benefice Commercial': 'الربح حسب العامل',
+        'Benefice Client': 'الربح حسب الزبون',
+        'Benefice Article': 'الربح حسب السلع',
+        'Benefice Category': 'الربح حسب الأصناف',
+        'Benefice CA': 'رقم الأعمال و الأرباح',
+
+        'Creance Cumule': 'الديون المتراكمة',
+        'Creance Client': 'ديون العملاء',
+
+        'User Profile': 'المستخدم',
+        'Settings': 'إعدادات',
+        'Logout': 'خروج',
+        'Close': 'غلق',
+        'Save': 'حفظ',
+
+        'Percentage': 'النسبة',
+
+        'All Years': 'كل السنوات',
+        'Performance': 'الكفاءة',
+        'About Description': 'تم تصميم  Dashboard Northwind كأنموذج لمنصة عرض البيانات مستندة في ذلك على جدول معلومات بصيغة JSON',
+
+        'info!': 'إفادة!',
+        'for more detail, try': 'للمزيد، جرّب',
+        'for more detail, select a year': 'للمزيد، إختر سنة',
+        'to filter, select Year / Month': 'للإنتقاء، إختر سنة \ شهر',
+        'to personalise language, theme, charts, select settings': 'لتغيير اللغة، الألوان، الرسوم البيانية، توجه للإعدادات',
+
+
+
+
+    };
+})(jQuery);
